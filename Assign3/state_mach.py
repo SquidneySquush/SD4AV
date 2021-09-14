14cec53bf90d9547086df44bcd71eda3361b8052
SquidneySquush <64729009+SquidneySquush@users.noreply.github.com>
2021-09-14 09:57:54 -0400

    Update state_mach.py

--- Assign3/state_mach.py
+++ Assign3/state_mach.py
@@ -15,5 +15,42 @@ from Assign3.Land import land
 from Assign3.Disarming import disarm
 
 
-# define state Mission Configuration
+# main
+def main():
 
+    rospy.init_node('smach_turtle_dance')
+
+    # Create a SMACH state machine
+    sm = smach.StateMachine(outcomes=['do_exit'])
+
+    # Open the container
+    with sm:
+        # Add states to the container
+        smach.StateMachine.add('MissConfig', MissConfig(), 
+                               transitions={'ConfigComp':'GrndInact'})
+        smach.StateMachine.add('GrndInact', GrndInact(), 
+                                transitions={'preCheckInit':'preCheck'})
+        smach.StateMachine.add('preCheck', preCheck(), 
+                               transitions={'c_passed':'Standby','c_failed':'GrndInact'})
+        smach.StateMachine.add('Standby', Standby(), 
+                               transitions={'rec_launch':'Arm'})
+        smach.StateMachine.add('Arm', Arm(), 
+                               transitions={'Armed':'takeoff','Arm_failed':'GrndInact'})
+        smach.StateMachine.add('takeoff', takeoff(), 
+                               transitions={'takeoff_abort':'land','alt_reach':'circle'})
+        smach.StateMachine.add('circle', circle(), 
+                               transitions={'recalled':'RHome'})
+        smach.StateMachine.add('RHome', RHome(), 
+                               transitions={'reach_home':'land'})
+        smach.StateMachine.add('land', land(), 
+                               transitions={'landed':'disarm'})
+        smach.StateMachine.add('disarm', disarm(), 
+                               transitions={'disarmed':'GrndInact'})
+
+
+    # Execute SMACH plan
+    outcome = sm.execute()
+
+
+if __name__ == '__main__':
+    main()
